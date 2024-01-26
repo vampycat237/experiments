db45d6bf8634260b4fa96690042e7deaea4a8290
vampycat237 <98717915+vampycat237@users.noreply.github.com>
2024-01-26 00:39:25 -0700

    added detailedTimeSinceRecorded()
    gives a more detailed statement of how long it's been since / will be until the recorded time

--- js/clock.js
+++ js/clock.js
@@ -54,6 +54,39 @@ function timeSinceRecorded() {
     return timeSince + timeUnit + modifier;
 }
 
+function detailedTimeSinceRecorded() {
+    var milliSecSince = currentTime.valueOf() - recordedTime.valueOf();
+    var secondsSince = Math.round(milliSecSince / 1000);
+    var modifier = " since";
+	
+    // days, hours, minutes, seconds
+    var timez = [ 0, 0, 0, 0 ]
+	
+    //resolve negatives with the modifier
+    if (secondsSince < 0) {
+        secondsSince = secondsSince * -1;
+        modifier = " until";
+    }
+	
+    // SECONDS
+    timez[3] = secondsSince % 60;
+
+    // DAYS  
+    timez[0] = Math.floor(secondsSince / (60 * 60 * 24));
+	secondsSince -= timez[0] * (60 * 60 * 24);
+	
+    // HOURS  
+    timez[1] = Math.floor(secondsSince / (60 * 60));
+	secondsSince -= timez[1] * (60 * 60);
+	
+    // MINUTES
+    timez[2] = Math.floor(secondsSince / 60);
+	secondsSince -= timez[2] * (60);
+	
+	
+    return `${timez[0]} days, ${timez[1]} hours, ${timez[2]} minutes, and ${timez[3]} seconds ${modifier}`;
+}
+
 function setRecordedClock(datetime) {
     recordedTime = new Date(datetime);
     console.log(datetime);
