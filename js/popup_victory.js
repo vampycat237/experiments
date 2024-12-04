16bc597aea069d394364c2d14652603b560501c6
vampycat237 <98717915+vampycat237@users.noreply.github.com>
2024-12-04 15:19:03 -0700

    preparing victory popups
    - created the framework for victory popups in its own script bc i like compartmentalization these days
    - they'll rotate through different congratulatory phrases
    - framework is semi-set up to comment on player achievements, however there is no tracking of related values yet

---
+++ js/popup_victory.js
@@ -0,0 +1,77 @@
+class VictoryPopUp {
+    windowTitles = [
+        "awesome!",
+        "congratz!",
+        "super!",
+        "swag!",
+        "yippee!",
+        ":D",
+        "winner!"
+    ];
+    msgs = [
+        "you completed the level!",
+        "truly, you are the organizer.",
+        "you water sorter, you.",
+        "i knew you could do it.",
+        "everybody look out, we got a water sorter in here!",
+        "swagtastic!"
+    ];
+    msgChallenges = [
+        "and you didn't use any ${BUTTON}s!",
+        "without having to ${BUTTON} at all!",
+        "${BUTTON} button? never heard of it."
+    ];
+
+    /** limit by windowTitles.length */
+    #titleIndex = [0, this.windowTitles.length];
+    /** limit by msgs.length */
+    #msgIndex = [0, this.msgs.length];
+    /** limit by msgChallenges.length */
+    #msgChallengeIndex = [0, this.msgChallenges.length];
+    /** limit by 3 */
+    #randomIndex = [0, 3];
+
+    /** Gets a usable index from a paired one. */
+    get(index) {
+        return index[0]%index[1];
+    }
+
+    getMsg() {
+        var msg = this.msgs[this.get(this.#msgIndex)];
+        this.#msgIndex[0]++;
+        return msg;
+    }
+    
+    getTitle() {
+        var title = this.windowTitles[this.get(this.#titleIndex)];
+        this.#titleIndex[0]++;
+        return title;
+    }
+
+    open() {
+        // Open with current data
+        $('#popup-victory-button').show();
+        openPopUp(this.getMsg(), this.getTitle());
+        // Fetching those values increments them afterwards, so we don't have to worry about that.
+
+        // 25% chance to randomly increment something a second time
+        if (Math.random() <= 0.25) {
+            switch (this.get(this.#randomIndex)) {
+                case 0:
+                    this.#titleIndex[0]++;
+                    break;
+                case 1:
+                    this.#msgIndex[0]++;
+                    break;
+                case 2:
+                    this.#msgChallengeIndex[0]++;
+                    break;
+            }
+
+            // then, increment iRandom (looping around if too large)
+            this.#randomIndex[0]++;
+        }
+    }
+}
+
+const vPopUp = new VictoryPopUp;
