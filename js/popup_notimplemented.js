9017245b90eb7edd862923bdcdba420bf7c92975
vampycat237 <98717915+vampycat237@users.noreply.github.com>
2024-12-04 16:29:01 -0700

    changed some window titles + not implemented popup
    - made the directory nicer to look at & easier to find the info popup
    - re-arranged clock buttons
    - created a new popup type for when a feature is not implemented. yes, the popup itself is implemented. wouldn't be very useful otherwise
    - made window titles a bit more consistent

---
+++ js/popup_notimplemented.js
@@ -0,0 +1,3 @@
+function notImplementedPopUp(featureName, wasHidden = false) {
+    openPopUp(`${featureName} is not implemented yet, sorry!<br><br>that's why it was hidden!`, 'not implemented');
+}
