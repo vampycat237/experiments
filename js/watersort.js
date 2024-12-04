165fd8c6cc26dbd4f0d1e78e1eff9a84a78be0f5
vampycat237 <98717915+vampycat237@users.noreply.github.com>
2024-12-04 15:38:35 -0700

    victory prompts new puzzle + massive code cleanup
    - implemented new victory popup code. the popup will now include a "next puzzle" button. hooray! #8
    - Removed out some uncessary html object grabs
    - Improved documentation significantly
    - Improved efficiency of several functions slightly... I think? (Fewer string concats, optimized some logic...) (#6)
    - Updated most document.getWhatever calls to use jquery instead
    - If the last tube you selected was empty, and you click on a different tube, the game will now correctly select the new tube and disregard the old, empty tube. (Previously, it deselected both tubes, which is annoying!) (Might help with #6)
    
    - WaterTube class now:
      - Tracks size (formerly fullness/howFull()) more sensibly, hopefully a little faster
      - Validates size of contents during construction (halfheartedly, but better than nothing)
      - Is more clear in its logic thanks to isEmpty(), isFull(), and other size methods/fields
      - Checks for completion in a way I actually understand, and may be more efficient?
      - Converts itself to HTML in-house

--- js/watersort.js
+++ js/watersort.js
@@ -1,9 +1,9 @@
 //wanna see how fast i can make a water color sort clone? too bad you're gonna
 //HTML object holder
-const watersort = {
+/*const watersort = {
     container: document.getElementById("watersort"),
     controls: document.getElementById("controls")
-}
+}*/
 
 //Static variables
 //Matches the number representing the water type (as the index) to the css variable to determine its color. Unused right now but can be used for customization later!
@@ -14,7 +14,10 @@ const waterPresets = {
     monochrome: ['#fff', '#ddd', '#bbb', '#999', '#777', '#555'],
     warm:    ['#a939b3', '#55aab3', '#a8c229', '#ffcd57', '#f5850f', '#db3e3e']
     }
-//Holds the WaterTubes in play.
+/** 
+ * Holds the WaterTubes in play. 
+ * @type WaterTube[]
+ */
 var waterTubes = [];
 //Tracks what tube is selected.
 var selectedTube = -1;
@@ -24,10 +27,22 @@ var levelStartSave;
 //CLASS DEFINITIONS
 //Water Tube class.
 class WaterTube {
-    //Contents should be an array with 4 values, like [1, 1, 1, 1]. The first value is the bottom, and the last is the top.
+    /** 'Size' of the tube. Represents how many non-empty water slots this WaterTube holds. */
+    #size = 0;
+    /** Maximum amount of water any WaterTube can hold. */
+    static #maxSize = 4;
+
+    /**
+     * Contents should be an array with 4 values, like [1, 1, 1, 1].
+     * The first value is the bottom, and the last is the top.
+     * 
+     * @param contents {String|String[]}
+     * @param ID {Number|null}
+     */
     constructor(contents, ID = null) {
         //Array contents: Assign immediately!
-        if (typeof contents == "object") {
+        if (typeof contents == "object" && contents.length <= WaterTube.#maxSize) {
+            /** @type String[] */
             this.contents = contents;
         }
         //"Encrypted"/ stored contents: Parse!
@@ -37,44 +52,75 @@ class WaterTube {
         }
 
         if (ID != null) {
+            /** @type Number */
             this.ID = ID;
         }
         else {
-            //Decide ID to be length of waterTubes
+            //Assume we are the next tube in waterTubes, therefore our ID should be its length (its last index + 1)
             this.ID = waterTubes.length;
         }
+
+        this.updateSize();
+
         //TODO: fetch water parts and assign them their numbers
         //TODO: randomize contents?
         //TODO: saving/loading contents, maybe based on seeds
     }
 
-    //Returns a number between 0 and 4 representing how full this tube is. 0 is empty, 4 is full.
-    howFull() {
-        var fullness = 0;
+    /** Returns true if the tube is full. */
+    isFull() {
+        return this.#size >= WaterTube.#maxSize;
+    }
+
+    /** Returns true if the tube is empty. */
+    isEmpty() {
+        return this.#size <= 0;
+    }
+
+    /** Calculates and returns the size of this tube. Returns how many non-empty water slots this tube has. */
+    updateSize() {
+        // Uses the private size variable
+        this.#size = 0;
         for (let v of this.contents) {
             if (v > 0) {
-                fullness++;
+                this.#size++;
             }
         }
-        return fullness;
+        return this.#size;
     }
 
-    //Returns the water type of the highest water level in this tube.
+    /** Returns the water type of the highest water level in this tube.*/
     getTopWaterType() {
-        return this.contents[this.howFull() - 1];
+        return this.contents[this.#size - 1];
     }
 
-    //Determines whether a water type can be added to this tube. The tube must have at least 1 open slot, and it must be of a matching type to the highest exposed water level.
+    /** 
+     * Returns whether the given water type can be added to this tube.
+     * 
+     * The tube must have at least 1 open slot, and the given water type
+     * must match that of the highest exposed water level.
+     */
     canAdd(waterType) {
-        //plaintext: if this is not full AND the water matches the top water here, OR if this tube is empty.
-        return ((this.howFull() < 4) && (waterType == this.getTopWaterType()) || this.howFull() < 1);
+        // we check if empty first because it's faster to calculate
+        // TODO: Should we stop from trying to add empty water, or does it not matter?
+        return (
+            // if this tube is empty, OR...
+            this.isEmpty() || 
+            (
+                // this tube is NOT FULL, AND...
+                (!this.isFull()) && 
+                // the top water type matches the one we want to add.
+                (waterType == this.getTopWaterType())
+            ));
     }
 
     //Attempts to add a new level of water in the tube. Returns true if successful, false if it fails.
     addTopWater(waterType) {
         if (this.canAdd(waterType)) {
             //This gets the highest EMPTY level of the tube! It can't be higher than 4 because of the canAdd check.
-            this.contents[this.howFull()] = waterType;
+            this.contents[this.#size] = waterType;
+            // Update the size after successfully adding.
+            this.updateSize();
             return true;
         }
         else {
@@ -82,63 +128,102 @@ class WaterTube {
         }
     }
 
-    //Tries to pour this tube's top water into another tube. Returns true if successful, false if it fails.
-    //otherTube should be a WaterTube object!
+    /**Tries to pour this tube's top water into another tube.
+     * 
+     * Returns true if successful, false if it fails.
+     * @param otherTube {WaterTube} The tube we want to pour our top contents into.
+     * @param level {number} Used when calling recursively to pour the same watertype.
+     * */
     pourTo(otherTube, level = 0) {
-        if (level > 4) {
+        // Base case for recursion: We've looped through all of our contents, no need to check anymore.
+        if (level > WaterTube.#maxSize) {
+            // Technically, this loop fails, so we return false.
             return false;
         }
-        if (this.howFull() > 0 && otherTube.addTopWater(this.getTopWaterType())) {
+        
+        if (
+            // this tube is NOT empty, AND...
+            !this.isEmpty() && 
+            // we successfully add our water to the other tube
+            otherTube.addTopWater(this.getTopWaterType())
+        ) {
+            // At this point, this.topWater === otherTube.topWater
+            // Save the kind of water we just poured
             const typePoured = this.getTopWaterType();
-            //we poured to the other tube! get rid of the level from THIS tube now
-            this.contents[this.howFull() - 1] = 0;
+            // Get rid of our copy of topWater
+            this.contents[this.#size - 1] = 0;
+            this.updateSize();
 
-            //console.log("poured type "+typePoured+"!");
-            //IF our next level is the same as the former, try to pour again (recursively ig?)! This will allow pouring matching water much more efficiently. No need to do anything with the return though
+            // Check if we should try to pour more water recursively. (If our new top water still matches the other tube, try to pour.)
+            // We don't need the return value from the recursive call.
             if (typePoured == this.getTopWaterType()) this.pourTo(otherTube, level+1);
 
+            // We successfully poured at least 1 level of water.
             return true;
         }
         else {
-            //we didn't pour, so don't do anything but return false.
+            // We didn't pour any water, so we'll return false later.
+
+            // Logs for debugging purposes
+            const shouldLog = (level < 1);
             var msg = "couldn't pour! reason: ";
-            if (otherTube.howFull() > 4) {
+            if (otherTube.isFull()) {
                 msg += "recieving tube is too full";
             }
             else if (otherTube.getTopWaterType() != this.getTopWaterType()) {
                 msg += "water types don't match";
             }
-            else if (this.howFull() <= 0) {
+            else if (this.isEmpty()) {
                 msg += "pouring tube is empty";
             }
             else {
                 msg += "uncaught :(";
+                shouldLog = true;
             }
-            if (level < 1) console.log(msg);
+            if (shouldLog) console.log(msg);
 
+            // No water was poured.
             return false;
         }
     }
 
     //Checks if this tube is 100% full *and* all the same type of water!
     checkForCompletion() {
-        const isFull = (this.howFull() >= 4);
-        const allMatching = (this.contents.every((val, i, arr) => val == arr[0])); //Pulled this from stack overflow, I don't understand a bit of it haha. source: https://stackoverflow.com/questions/14832603/check-if-all-values-of-array-are-equal
+        this.updateSize();
 
-        const isComplete = (isFull && allMatching);
+        // Cut out some work by failing early if isFull is false.
+        if (!this.isFull()) {
+            if (this.isEmpty()) {
+                console.log(this.ID + " incomplete: is empty");
+            }
+            else console.log(this.ID + " incomplete: not full");
+            return false;
+        }
 
-        /*var msg = "cringe fail!! reason: ";
-        if (!allMatching) msg += "contents dont match. ";
-        else if (!isFull) msg += "tube not full. ";
-        else if (!isComplete) msg += "a secret third thing. ";
+        // Old allMatching code
+        //const allMatching = (this.contents.every((val, i, arr) => val == arr[0])); //Pulled this from stack overflow, I don't understand a bit of it haha. source: https://stackoverflow.com/questions/14832603/check-if-all-values-of-array-are-equal
 
-        if (!isComplete) {
-            msg += "sent from tube " + this.ID;
-            console.log(msg);
-        }*/
 
-        //console.log((this.contents.every((val, i, arr) => val == arr[0])));
-        return isComplete;
+        // check if all water in tube matches...
+        // Declare a variable which assumes the water matches. (We know the tube isn't empty, so we don't have to worry about false positives.)
+        var allMatching = true;
+        // Save the top water type.
+        const waterType = this.getTopWaterType();
+        // Loop thru tube.
+        for (let water of this.contents) {
+            // If any level of water doesn't match what's on top, then obviously our tube is not all matching.
+            if (water != waterType) {
+                console.log(this.ID + " incomplete: not matching itself");
+                allMatching = false;
+                break;
+            }
+        }
+        // If all levels of water matched what was on top, then they all match each other, so we are all matching.
+        // This might be faster than the previous code, IDK, but at least I understand it.
+
+        
+        // Since we already know isFull must be true, we just return allMatching.
+        return allMatching;
     }
 
     //Crunches the object into just its contents. Represented as #### (decimal)
@@ -157,13 +242,25 @@ class WaterTube {
 
     //Crunches the object into just its contents. Represented as ####
     toNum() {
+        if (this.isEmpty()) return 0;
         return parseInt(this.toString());
     }
 
+    toHTML() {
+        const str = [];
+        str.push(`<div class="watertube" id="${this.ID}" onclick="select(${this.ID})">`);
+        str.push(`   <div class="water water${this.contents[3]}"></div>`);
+        str.push(`   <div class="water water${this.contents[2]}"></div>`);
+        str.push(`   <div class="water water${this.contents[1]}"></div>`);
+        str.push(`   <div class="water water${this.contents[0]}"></div>`);
+        str.push(`</div>`);
+
+        return str.join("\n");
+    }
 }
 
-//STATIC METHODS
-//Fetches water colors from CSS. Returns them as an array
+//STATIC FUNCTIONS
+/** Fetches water colors from CSS. Returns them as an array */
 function getWaterColors() {
     const computedStyles = getComputedStyle(document.querySelector(':root'));
 
@@ -175,7 +272,11 @@ function getWaterColors() {
     console.log(waterTypeKey);
 }
 
-//Sets water colors in CSS based on given colors array. Do not include water_0 - array should be length 5
+/**
+ * Sets water colors in CSS based on given colors array.
+ * 
+ * Do not include water_0 - array should be length 5
+ */
 function setWaterColors(presetName) {
     const root = document.querySelector(':root');
     for (let i = 1; i <= 6; i++) {
@@ -184,12 +285,14 @@ function setWaterColors(presetName) {
     
 }
 
-//TODO
-//Generates a random level.
+/** Generates a random level. */
 function generateLevel() {
     console.log("the man behind the water"); //silly :3 (improved silliness thanks to my friend finalfloof)
-    //Clears the previous tubes
+    
+    //Clear the previous tubes
+    // TODO: Do we need to deliberately dispose of those tubes?
     waterTubes = [];
+
     //Array that represents how many of the various colors we have "left". Starts at 4 for each.
     const waterColors = [4, 4, 4, 4, 4, 4];
 
@@ -245,8 +348,8 @@ function generateLevel() {
 function saveLevel() {
     const saveArray = [];
     for (tube of waterTubes) {
-        if (tube.howFull() > 0) {
-            //Tube is not empty! We save it.
+        // Only save non-empty tubes
+        if (!tube.isEmpty()) {
             saveArray.push(tube.toString());
         }
     }
@@ -266,10 +369,13 @@ function loadLevel(key) {
         waterTubes.push(new WaterTube(tube));
     }
 
+    // Empty tubes aren't included in save keys, so make sure to add them.
     addEmptyTube(2)
 }
 
-//Adds empty tubes.
+/** Adds one or more empty tubes to the playspace.
+ * @param tubesCount {number} How many empty tubes to add.
+*/
 function addEmptyTube(tubesCount = 1) {
     for (let i = 0; i < tubesCount; i++) {
         waterTubes.push(new WaterTube([0, 0, 0, 0]));
@@ -277,7 +383,7 @@ function addEmptyTube(tubesCount = 1) {
     render();
 }
 
-//Update the HTML to reflect the game!
+/** Updates the HTML to reflect the game!*/
 function render() {
     /* TEMPLATE
     <div class="watertube">
@@ -287,40 +393,41 @@ function render() {
         <div class="water bottom"></div>
     </div>
     */
-    var display = '';
+    var display = [];
+    // TODO: Target individual tubes for updates, instead of instantializing the whole playspace...
 
+    //console.log(waterTubes);
     for (tube of waterTubes) {
-        display += '<div class="watertube" id="'+ tube.ID +'" onclick="select('+ tube.ID +')">';
-
-        display += '<div class="water water' + tube.contents[3] + '"></div>';
-        display += '<div class="water water' + tube.contents[2] + '"></div>';
-        display += '<div class="water water' + tube.contents[1] + '"></div>';
-        display += '<div class="water water' + tube.contents[0] + '"></div>';
-
-        display += '</div>';
+        //console.log(tube);
+        //console.log(tube.toHTML());
+        display.push(tube.toHTML());
     }
 
-    watersort.container.innerHTML = display;
+    $("#watersort")[0].innerHTML = display.join("\n");
 
     checkForWin();
 }
 
-//Changes the "selected" tube to the given one!
+/** Changes the "selected" tube to the given one! */
 function select(i) {
-    //console.log("selection going from " + selectedTube + " to " + i);
+    const selector = '#'+i;
+    
+    // selecting same tube
     if (i == selectedTube) {
         //same tube as before? deselect and set selected to invalid
-        document.getElementById(i).classList.remove("selected");
+        $(selector)[0].classList.remove("selected");
 
         selectedTube = -1;
     }
+    // previously had nothing selected
     else if (selectedTube == -1) {
         //tube was previously invalid? select the new tube
-        document.getElementById(i).classList.add("selected");
+        $(selector)[0].classList.add("selected");
 
         selectedTube = i;
     }
-    else if (waterTubes[selectedTube].howFull() <= 0) {
+    // previously had empty tube selected
+    else if (waterTubes[selectedTube].isEmpty()) {
         //was selecting an empty tube before, so probably wants to switch to selecting this new tube instead since can't pour.
         //document.getElementById(selectedTube).classList.remove("selected");
 
@@ -328,45 +435,64 @@ function select(i) {
             elmnt.classList.remove("selected");
         }
 
-        document.getElementById(i).classList.add("selected");
+        $(selector)[0].classList.add("selected");
 
-        //deselect tubes
-        selectedTube = -1;
+        //Select new tube
+        selectedTube = i;
     }
+    // otherwise, it's gotta be two different tubes.
     else {
-        //two different tubes! try pour
+        // Try to pour from the formerly selected tube to the newly selected tube.
         waterTubes[selectedTube].pourTo(waterTubes[i]);
 
-        for (elmnt of document.getElementsByClassName("selected")) {
+        for (elmnt of $(".selected")) {
             elmnt.classList.remove("selected");
         }
         //document.getElementById(selectedTube).classList.remove("selected");
 
-        //deselect tubes
+        // TODO: At this moment, we have gamecode updated, and IDs of both changed tubes.
+        // This would be the perfect time to update the INDIVIDUAL tubes in HTML instead of a whole render function.
+
+        // Deselect tubes
         selectedTube = -1;
+        // Render changes
+        // If call to render is removed, consider calling checkForWin in this function.
         render();
     }
 }
 
-//Checks for win conditions! Win condition: Every tube is either (100% full of the same color) or (empty).
+/** 
+ * Checks for win conditions!
+ * 
+ * Win condition: Every tube is either (100% full of the same color) or (empty).
+ */
 function checkForWin() {
     var allComplete = true;
     for (tube of waterTubes) {
-        if (!(tube.checkForCompletion()) && tube.howFull() > 0) {
+        // We're looking for FAIL conditions here.
+        // All tubes should be either COMPLETE or EMPTY. If neither applies, we fail.
+        if (!(tube.checkForCompletion() || tube.isEmpty())) {
             //This tube NOT all matched!
-            //console.log(`tube: ${tube.ID}. complete: ${tube.checkForCompletion()}. not empty: ${tube.howFull() > 0}`);
+            console.log("win checker failed at "+tube.ID);
             allComplete = false;
             break;
         }
     }
     if (allComplete) {
-        openPopUp("you completed the level!");
+        victoryPopUp();
     }
 }
 
+/** Shows the victory popup screen. */
+function victoryPopUp() {
+    // This is in its own script.
+    vPopUp.open();
+}
+
+/** Runs on page load. */
 function main() {
     getWaterColors();
     generateLevel();
     //const testTube = new WaterTube([1,1,1,1]);
-    //console.log(testTube.howFull());
+    //console.log(testTube.updateSize());
 }
